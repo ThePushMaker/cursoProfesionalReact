67e3e4f6f5a5dbe7a15774b6fdea06674a7fc0d9
DSW <elmcwl@gmail.com>
2023-08-25 23:37:24 -0700

    feat: add Events and EventsItems components

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,4 +1,10 @@
-const EventItem = () => {
-  return <div>EventItem</div>
+const EventItem = ({ info, name, image }) => {
+  return (
+      <div>
+        <img src={image} alt={name} width={200} height={200} />
+        <h4>{name}</h4>
+        <p>{info}</p>
+      </div>
+    );
 }
 export default EventItem;
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 import EventItem from "./components/EventItem";
+import eventjsJSON from '../../data/events.json';
+console.log(10);
 
 const Events = () => {
-
+  const [data, setData] = useState(eventjsJSON); 
+  console.log(data);
+  const { _embedded: { events }} = data;
+  
+  const eventsComponent = events.map((eventItem) => ( 
+      <EventItem 
+        key={`event-item-${eventItem.id}`} 
+        name={eventItem.name}
+        info={eventItem.info}
+        image={eventItem.images[0].url}
+      />
+  ));
   return(
     <div>
       Events
-      <EventItem />
+      {eventsComponent}
     </div>
   );
 }
