7ec3e447754a0a6bdcca7ff030b74edc0ea7d67e
DSW <elmcwl@gmail.com>
2023-10-20 22:52:01 -0700

    feat: event propagation

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,10 +1,15 @@
 const EventItem = ({ info, id, name, image, onEventClick }) => {
+  const handleSeeMoreClick = (evt) => {
+    evt.stopPropagation();
+    onEventClick(id);
+  }
+  
   return (
-      <div>
+      <div onClick={() => console.log("padre clickeado")}>
         <img src={image} alt={name} width={200} height={200} />
         <h4>{name}</h4>
         <p>{info}</p>
-        <button onClick={() => onEventClick(id)}>Ver más</button>
+        <button onClick={handleSeeMoreClick}>Ver más</button>
       </div>
     );
 }
