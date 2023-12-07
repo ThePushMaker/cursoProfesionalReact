c5c206d7537ade16d0211da107f43ad32044561d
Martin Calderon <elmcwl@gmail.com>
2023-12-07 02:39:48 -0700

    feat: fixed bug in buttons form

--- src/components/SignupForm/index.jsx
+++ src/components/SignupForm/index.jsx
@@ -8,38 +8,49 @@ const SignupForm = () => {
   const [phone, setPhone] = useState('');
   
   const handleClearClick = () => {
+    console.log('clear');
     
   };
   
-  const handleSubmitForm = () => {
+  const handleSubmitForm = (evt) => {
+    evt.preventDefault();
     
+  
+    
+    console.log('submit', {
+                name, 
+                age, 
+                address, 
+                zipcode, 
+                phone
+    });
   };
   
   return (
     <form onSubmit={handleSubmitForm} style={{display:"flex", flexDirection:"column"}}>
       <label>
         Name
-        <input value={name} onChange={(evt) => setName(evt.target.value)} />
+        <input value={name} onChange={(evt) => setName(evt.target.value)} required />
       </label>
       <label>
         Age
-        <input value={age} onChange={(evt => setAge(evt.target.value))} />
+        <input value={age} onChange={(evt => setAge(evt.target.value))} required />
       </label>
       <label>
         Address
-        <input value={address} onChange={(evt) => setAddress(evt.target.value)}/>
+        <input value={address} onChange={(evt) => setAddress(evt.target.value)} required />
       </label>
       <label>
         Zipcode
-        <input value={zipcode} onChange={(evt) => setZipcode(evt.target.value)} />
+        <input value={zipcode} onChange={(evt) => setZipcode(evt.target.value)} required />
       </label>
       <label>
         Phone
-        <input value={phone} onChange={(evt) => setPhone(evt.target.value)} />
+        <input value={phone} onChange={(evt) => setPhone(evt.target.value)} required />
       </label>
       
-      <div>
-        <button onClick={handleClearClick}>Clear</button>
+      <div> 
+        <button type="button" onClick={handleClearClick}>Clear</button>
         <button type="submit">Submit</button>
       </div>
     </form>
